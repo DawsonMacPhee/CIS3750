5975c8a0dc62573e28e25cb70063132851ba6e8e
DawsonMacPhee <dawsonmacphee@icloud.com>
2022-03-26 13:44:43 -0400

    Finialized Keycloak integration

--- semtech/src/router/index.ts
+++ semtech/src/router/index.ts
@@ -28,26 +28,33 @@ router.beforeEach((to, from, next) => {
     if (to.meta.isAuthenticated) {
         const keycloak = app.config.globalProperties.$keycloak;
         const basePath = window.location.toString()
-        if (!keycloak.authenticated) {
-            console.log("Logging In");
-            keycloak.login({ redirectUri: basePath.slice(0, -1) + to.path })
-        } else if (keycloak.hasRealmRole('user')) {
-            console.log("Authenticated");
-            keycloak.updateToken(70)
-            .then(() => {
-                next()
-            })
-            .catch(err => {
-                console.error(err)
-            })
+
+        if (keycloak.ready != null && keycloak.ready == false) {
+            next({name: "home"})
         } else {
-            console.log("Wrong Permissions");
-            next({ name: 'Unauthorized' })
+            if (!keycloak.authenticated) {
+                console.log("Logging In");
+                keycloak.login({ redirectUri: basePath.split("/")[0] + to.path });
+            } else if (keycloak.hasRealmRole('user')) {
+                console.log("Authenticated");
+                keycloak.updateToken(70)
+                .then(() => {
+                    next()
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+            } else {
+                console.log("Wrong Permissions");
+                next({name: "home"})
+            }
         }
+
     } else {
         console.log("No Auth");
         next()
     }
+
 })
 
 export default router
